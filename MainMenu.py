daec07aec107989105a4784a85c5702350ad2946
MatthewSoars <matthewsoars2004@gmail.com>
2021-10-21 09:58:36 +0100

    Update as of 21/10/2021 at 09:57am working on the main menu finished adding the buttons and working on interactivity, next thing to do is to add a 'player sprite' to make the screen feel more alive

--- MainMenu.py
+++ MainMenu.py
@@ -1,6 +1,7 @@
 # Imports the libraries needed for the screen to work
 import pygame
 
+
 ScreenHeight = 1366  # Sets the screens height
 ScreenWidth = 768  # Sets the screens width
 
@@ -8,10 +9,13 @@ ScreenWidth = 768  # Sets the screens width
 pygame.init()  # initializing the imported module
 MM = pygame.display.set_mode((ScreenHeight, ScreenWidth))  # Displaying a window of set parameters
 
-titleFont = pygame.font.SysFont('Comic Sans MS', 200)
-buttonFont = pygame.font.SysFont('Comic Sans MS', 35)
-textsurface = titleFont.render('Birdy', False, (0, 0, 0))
-startGameButton = buttonFont.render('Start', True, (0, 0, 0))
+titleFont = pygame.font.SysFont('Comic Sans MS', 200)  # Sets the font and the size of font to be used for the title
+buttonFont = pygame.font.SysFont('Comic Sans MS', 35)  # Sets the font and the size of font to be used for the buttons
+textsurface = titleFont.render('Birdy', False, (0, 0, 0))  # Sets the parameters for the title to be called later
+startGameButton = buttonFont.render('Start', True, (0, 0, 0))  # Sets parameters for the start button to be called later
+statsGameButton = buttonFont.render('Stats', True, (0, 0, 0))  # Sets parameters for the stats button to be called later
+shopGameButton = buttonFont.render('Shop', True, (0, 0, 0))  # Sets parameters for the shop button to be called later
+settingsGameButton = buttonFont.render('Settings', True, (0, 0, 0))  # Sets parameters for setting button for later
 
 backGroundImage = pygame.image.load("Sprites/Background5.jpg").convert()  # Loading the background game sprite
 backGroundPosition = 0  # Setting the backgrounds original position
@@ -20,7 +24,7 @@ running = True  # Sets the running boolean to true
 # Game loop while the program is running
 while running:
 
-    mousePosition = pygame.mouse.get_pos()
+    mousePosition = pygame.mouse.get_pos()  # Gets the current position of the mouse and stores it
 
     # This section of code uses two backgrounds in order to create an infinite effect
     relativeBackGroundPosition = backGroundPosition % backGroundImage.get_rect().width  # Uses mod to move background
@@ -29,48 +33,82 @@ while running:
         MM.blit(backGroundImage, (relativeBackGroundPosition, 0))  # Blits the second background image to the screen
     backGroundPosition -= 1  # Makes the background shift to the left
 
-    MM.blit(textsurface, (450, 20))
+    MM.blit(textsurface, (450, 20))  # Displays the title of the game to the screen
 
     for event in pygame.event.get():  # Checks to see if any event in queue
 
         if event.type == pygame.QUIT:  # If event is quit
             running = False  # Set running bool to false
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if ScreenWidth / 2 <= mousePosition[0] <= ScreenWidth and ScreenHeight / 2 <= mousePosition[1]:
-                pygame.quit()
+        # Next section of event recognition refers to the detection of button presses
+        # This is for the game button calling the game to play
+        if event.type == pygame.MOUSEBUTTONDOWN:  # Detects if the mouse button has been pressed
+            if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+                if 270 <= mousePosition[1] <= 270 + 80:  # Detects if mouse position y is within set bounds
+                    print("Game")  # This is where the code calling the game to play goes
+
+        # This is for the game button calling the shop menu
+        if event.type == pygame.MOUSEBUTTONDOWN:  # Detects if the mouse button has been pressed
+            if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+                if 370 <= mousePosition[1] <= 370 + 80:  # Detects if mouse position y is within set bounds
+                    print("Shop")  # This is where the code calling the shop menu
+
+            # This is for the game button calling the stats menu
+            if event.type == pygame.MOUSEBUTTONDOWN:  # Detects if the mouse button has been pressed
+                if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+                    if 470 <= mousePosition[1] <= 470 + 80:  # Detects if mouse position y is within set bounds
+                        print("Stats")  # This is where the code calling the stats menu
+
+            # This is for the game button calling the settings menu
+            if event.type == pygame.MOUSEBUTTONDOWN:  # Detects if the mouse button has been pressed
+                if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+                    if 570 <= mousePosition[1] <= 570 + 80:  # Detects if mouse position y is within set bounds
+                        print("Settings")  # This is where the code calling the settings menu
 
     # This next section of code relates to the play button detection of the mouse when hovered over
-    if 550 <= mousePosition[0] <= 550 + 280:
-        if 350 <= mousePosition[1] <= 350 + 80:
-            pygame.draw.rect(MM, (170, 170, 170), [550, 350, 280, 80])
+    if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+        if 270 <= mousePosition[1] <= 270 + 80:  # Detects if mouse position y is within set bounds
+            pygame.draw.rect(MM, (170, 170, 170), [550, 270, 280, 80])  # Draws a lighter rectangle when hovered over
 
         else:
-            pygame.draw.rect(MM, (100, 100, 100), [550, 350, 280, 80])
+            pygame.draw.rect(MM, (100, 100, 100), [550, 270, 280, 80])  # Draws a darker rectangle when not hovered over
     else:
-        pygame.draw.rect(MM, (100, 100, 100), [550, 350, 280, 80])
+        pygame.draw.rect(MM, (100, 100, 100), [550, 270, 280, 80])  # Draws a darker rectangle when not hovered over
 
     # This next section of code relates to the shop menu button detection of the mouse when hovered over
-    if 550 <= mousePosition[0] <= 550 + 280:
-        if 450 <= mousePosition[1] <= 450 + 80:
-            pygame.draw.rect(MM, (170, 170, 170), [550, 450, 280, 80])
+    if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+        if 370 <= mousePosition[1] <= 370 + 80:  # Detects if mouse position y is within set bounds
+            pygame.draw.rect(MM, (170, 170, 170), [550, 370, 280, 80])  # Draws a lighter rectangle when hovered over
 
         else:
-            pygame.draw.rect(MM, (100, 100, 100), [550, 450, 280, 80])
+            pygame.draw.rect(MM, (100, 100, 100), [550, 370, 280, 80])  # Draws a darker rectangle when not hovered over
     else:
-        pygame.draw.rect(MM, (100, 100, 100), [550, 450, 280, 80])
+        pygame.draw.rect(MM, (100, 100, 100), [550, 370, 280, 80])  # Draws a darker rectangle when not hovered over
 
     # This next section of code relates to the stats menu button detection of the mouse when hovered over
-    if 550 <= mousePosition[0] <= 550 + 280:
-        if 550 <= mousePosition[1] <= 550 + 80:
-            pygame.draw.rect(MM, (170, 170, 170), [550, 550, 280, 80])
+    if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+        if 470 <= mousePosition[1] <= 470 + 80:  # Detects if mouse position y is within set bounds
+            pygame.draw.rect(MM, (170, 170, 170), [550, 470, 280, 80])  # Draws a lighter rectangle when hovered over
 
         else:
-            pygame.draw.rect(MM, (100, 100, 100), [550, 550, 280, 80])
+            pygame.draw.rect(MM, (100, 100, 100), [550, 470, 280, 80])  # Draws a darker rectangle when not hovered over
     else:
-        pygame.draw.rect(MM, (100, 100, 100), [550, 550, 280, 80])
+        pygame.draw.rect(MM, (100, 100, 100), [550, 470, 280, 80])  # Draws a darker rectangle when not hovered over
 
-    MM.blit(startGameButton, (650, 360))  # Super imposes the Start text on the start game button
-    MM.blit()
+    # This next section of code relates to the setting menu button detection of the mouse when hovered over
+    if 550 <= mousePosition[0] <= 550 + 280:  # Detects if mouse position x is within set bounds
+        if 570 <= mousePosition[1] <= 570 + 80:  # Detects if mouse position y is within set bounds
+            pygame.draw.rect(MM, (170, 170, 170), [550, 570, 280, 80])  # Draws a lighter rectangle when hovered over
+
+        else:
+            pygame.draw.rect(MM, (100, 100, 100), [550, 570, 280, 80])  # Draws a darker rectangle when not hovered over
+    else:
+        pygame.draw.rect(MM, (100, 100, 100), [550, 570, 280, 80])  # Draws a darker rectangle when not hovered over
+
+    # This next section relates to the super imposing of text onto the buttons previously created
+    MM.blit(startGameButton, (650, 290))  # Super imposes the start text on the start game button
+    MM.blit(shopGameButton, (655, 385))  # Super imposes the shop text on the start game button
+    MM.blit(statsGameButton, (650, 485))  # Super imposes the stats text on the start game button
+    MM.blit(settingsGameButton, (625, 585))  # Super imposes the settings text on the start game button
 
     pygame.display.update()  # Updates the screen
