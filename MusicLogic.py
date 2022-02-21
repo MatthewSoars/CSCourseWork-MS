aa0fc37a8b608e1e0a43ebaebf38db9cc106c53b
Matthew Soars <46872066+MatthewSoars@users.noreply.github.com>
2022-02-21 18:58:34 +0000

    Comments

--- MusicLogic.py
+++ MusicLogic.py
@@ -1,7 +1,7 @@
 # Imports the libraries needed for the screen to work
 import pygame  # Imports the pygame library
 
-
+# Code for the class of the music logic
 class LogicMusic:
     def __init__(self):  # Function when class is instantiated
         list_of_music = ["Sounds/Music/Figure It Out - 8bit - Royal Blood (320 kbps).mp3"]  # List of music to play
